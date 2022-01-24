d426451a3631236f88d2787d3bf03d31b68d287e
YUNYI <1184642524@qq.com>
2022-01-24 17:16:42 -0500

    After p38

---
+++ blog-admin/src/main/java/com/yunyi/blog/admin/mapper/PermissionMapper.java
@@ -0,0 +1,12 @@
+package com.yunyi.blog.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yunyi.blog.admin.pojo.Permission;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PermissionMapper extends BaseMapper<Permission> {
+
+    
+}
